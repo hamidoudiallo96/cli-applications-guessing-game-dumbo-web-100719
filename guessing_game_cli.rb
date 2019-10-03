1e58a0728c8d10817d95f91ac2781ff853c7f5b6
Hamidou Diallo <dio.hamidou@gmail.com>
2019-10-03 22:53:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@
 require ''
 
 def run_guessing_game
-  game_statement = ""
-  random_number = rand(1..6)
   puts  'Guess a number between 1 and 6'
+  random_number = rand(1..6)
   input = gets.chomp
+  
   
 end
