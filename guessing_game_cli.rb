a8959281c3c4592ca325a1fe1a1d8195a745ece4
Hamidou Diallo <dio.hamidou@gmail.com>
2019-10-03 23:07:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 def run_guessing_game
   random_number = rand(1..6)
-  puts  'Guess a number between 1 and 6'
+  puts  'Guess a number between 1 and 6:'
   input = gets.chomp
   if input == 'exit'
     puts "Goodbye!"
